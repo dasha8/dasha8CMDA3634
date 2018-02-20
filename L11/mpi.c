d5e19cad97fc17128a1e92bbb9ddd901cc332c49
Dasha <dasha8@vt.edu>
2018-02-20 16:34:27 -0500

    progress with mpi.c

--- L11/mpi.c
+++ L11/mpi.c
@@ -1,18 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include "mpi.h"
 
 int main(int argc, char **argv) {
+
+  MPI_Init(&argc,&argv);
+
+  int rank, size;
+  MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+  MPI_Comm_size(MPI_COMM_WORLD,&size);
 
   //need running tallies
   long long int Ntotal;
   long long int Ncircle;
 
   //seed random number generator
-  double seed = 1.0;
-  srand48(seed);
+  //double seed = 1.0;
+  srand48(rank);
 
-  for (long long int n=0; n<100000000; n++) {
+  for (long long int n=0; n<10000000; n++) {
     //generate two random numbers
     double rand1 = drand48(); //drand48 returns a number between 0 and 1
     double rand2 = drand48();
@@ -25,9 +32,23 @@ int main(int argc, char **argv) {
     Ntotal++;
   }
 
+  double sum;
+
   double pi = 4.0 * Ncircle/ (double) Ntotal;
 
-  printf("Our estimate of pi is %f \n", pi);
+  for (int i=0;i<100;i++) {
+    if (rank==0)  printf("Our estimate of pi is %f \n", pi);
+  }
+
+
+  MPI_Allreduce(&pi,
+	     &sum,
+	     1,
+	     MPI_FLOAT,
+	     MPI_SUM,
+	     MPI_COMM_WORLD);
+
+  MPI_Finalize();
 
   return 0;
 }
