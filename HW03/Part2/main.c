8f3b0b2ff7efb218e21861b607071c002837ef3b
Dasha Savina <dasha8@nrlogin2.cluster>
2018-03-26 18:09:52 -0400

    n=18 and n=24

--- HW03/Part2/main.c
+++ HW03/Part2/main.c
@@ -32,7 +32,7 @@ int main (int argc, char **argv) {
   if (rank == 0) {
     //printf("Enter a number of bits: "); fflush(stdout);
     //char status = scanf("%u",&n);
-    n = 24;
+    n = 30;
     
     //make sure the input makes sense
     if ((n<3)||(n>31)) {//Updated bounds. 2 is no good, 31 is actually ok
